042913b8fd89c6f575c0004dcdb7596cdca5cd3a
Maximilian Blochberger <blochberger@informatik.uni-hamburg.de>
2018-11-13 16:11:16 +0100

    Add stub for unit tests

---
+++ DCryptTests/DCryptTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import DCrypt
+
+class DCryptTests: XCTestCase {
+
+    func testExample() {
+		let dcrypt = DCrypt()
+
+		// <#TODO#> Test encryption and decryption
+    }
+
+}
